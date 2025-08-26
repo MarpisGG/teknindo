6076ac08032d952eede37835766ef940ffb86d5d
MarpisGG <marvellc83@gmail.com>
2025-08-26 13:38:34 +0700

    kintil

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: 'public/build', // HARUS BEGITU
+        outDir: 'public_html/build', // HARUS BEGITU
         manifest: true,
     },
 });
