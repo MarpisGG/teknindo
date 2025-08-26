a6490dc125b739c7c9480a1f30606d3ab8ae172f
MarpisGG <marvellc83@gmail.com>
2025-08-26 16:01:53 +0700

    siap

---
+++ public/build/assets/index-CmTb1Cml.js
@@ -0,0 +1,1 @@
+import{f as r,g as t}from"./app-BK4j1Hew.js";var a=r();const e=t(a);export{e as R,a as r};
--- resources/js/pages/landing.tsx
+++ resources/js/pages/landing.tsx
@@ -13,7 +13,6 @@ import { Head, Link, usePage } from '@inertiajs/react';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
-import video from '../../assets/video/videolanding.mp4';
 import HorizontalScrollCarouselDemo from './demo';
 
 interface Types {
@@ -59,7 +58,7 @@ export default function Welcome() {
                         loop
                         muted
                         className="absolute top-0 left-0 h-full w-full object-cover"
-                        src={video}
+                        src="/videos/videolanding.mp4"
                         style={{
                             transform: 'translateZ(-1px) scale(2)',
                             minHeight: '100vh',
